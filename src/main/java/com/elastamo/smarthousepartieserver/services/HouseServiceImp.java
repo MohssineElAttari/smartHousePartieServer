229bae5e8b88eec53dfb5837c42c27fdc7662e8d
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-04-13 23:23:31 +0000

    add HouseServiceImp 😁❤️✌️

---
+++ src/main/java/com/elastamo/smarthousepartieserver/services/HouseServiceImp.java
@@ -0,0 +1,46 @@
+package com.elastamo.smarthousepartieserver.services;
+
+import com.elastamo.smarthousepartieserver.models.House;
+import com.elastamo.smarthousepartieserver.repository.HouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HouseServiceImp implements HouseServiceInt{
+
+    final private HouseRepository houseRepository;
+    @Autowired
+    public HouseServiceImp(HouseRepository houseRepository){
+        this.houseRepository=houseRepository;
+    }
+
+    @Override
+    public House addHouse(House house) {
+        return houseRepository.save(house);
+    }
+
+    @Override
+    public List<House> getAll() {
+        return houseRepository.findAll();
+    }
+
+    @Override
+    public String delete(House house) {
+        houseRepository.delete(house);
+        return "Deleted!";
+    }
+
+    @Override
+    public House findById(String id) {
+        return houseRepository.findById(id).get();
+    }
+
+    @Override
+    public House update(House house, String id) {
+        House house1 =houseRepository.findById(id).get();
+        house1.setName(house.getName());
+        return houseRepository.save(house1);
+    }
+}
