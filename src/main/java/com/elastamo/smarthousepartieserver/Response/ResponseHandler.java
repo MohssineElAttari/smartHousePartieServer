13cca2e9622ecd9c5465f754a7048330b2178ddf
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-04-26 09:41:21 +0000

    add responsable handler

---
+++ src/main/java/com/elastamo/smarthousepartieserver/Response/ResponseHandler.java
@@ -0,0 +1,42 @@
+package com.elastamo.smarthousepartieserver.Response;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseHandler {
+    public static ResponseEntity<Object> generateResponse(String message , HttpStatus status, Object responseObject){
+
+        Map<String,Object> map = new HashMap<>();
+
+        map.put("message",message);
+
+        map.put("status",status.value());
+
+        map.put("data",responseObject);
+
+        return new ResponseEntity<Object>(map,status);
+
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public static ResponseEntity<Object> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<Object>(errors, null);
+    }
+}
