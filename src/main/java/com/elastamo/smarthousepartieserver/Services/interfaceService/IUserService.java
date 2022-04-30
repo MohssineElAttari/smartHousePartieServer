6e7a7f917029d50af5eb4100ec178229ab68d16c
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-04-30 23:44:59 +0000

    new update in IUserService

--- src/main/java/com/elastamo/smarthousepartieserver/Services/interfaceService/IUserService.java
+++ src/main/java/com/elastamo/smarthousepartieserver/Services/interfaceService/IUserService.java
@@ -1,15 +1,17 @@
 package com.elastamo.smarthousepartieserver.Services.interfaceService;
 
 import com.elastamo.smarthousepartieserver.Models.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public interface IUserService {
+public interface IUserService extends UserDetailsService {
     public User addUser(User user);
     public User update(User user);
     public String delete(String id);
+    public User findByUsername(String username);
 
     User findById(String id);
 
