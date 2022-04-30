ae457ee1c83f3afef4a2c3d6e4219bbbf5d0a4d3
Elattari Mohssine <43855033+MohssineElAttari@users.noreply.github.com>
2022-04-30 23:46:07 +0000

    new commit

--- src/main/java/com/elastamo/smarthousepartieserver/SmartHousePartieServerApplication.java
+++ src/main/java/com/elastamo/smarthousepartieserver/SmartHousePartieServerApplication.java
@@ -1,17 +1,25 @@
 package com.elastamo.smarthousepartieserver;
 
+import com.elastamo.smarthousepartieserver.Models.User;
 import com.elastamo.smarthousepartieserver.Repository.HouseRepository;
 import com.elastamo.smarthousepartieserver.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Scanner;
 
 @SpringBootApplication
+@RestController
 public class SmartHousePartieServerApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(SmartHousePartieServerApplication.class, args);
@@ -20,5 +28,18 @@ public class SmartHousePartieServerApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/user/{username}")
+    public User getUserBUsername(@PathVariable("username") String username) {
+        return this.userRepository.findByUsername(username);
     }
 }
